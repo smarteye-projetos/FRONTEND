8f466210031913bf63d84c2aec6dfb123e455f18
Junior Cunha <manoelricardo38@gmail.com>
2020-12-04 11:16:28 -0300

    Adicionando media diaria na camera

--- src/pages/Camera/components/Box/index.js
+++ src/pages/Camera/components/Box/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import { Container , Row} from './styles';
 import Card from '../Card';
 
 import GraficoLine from '../Graficos/GraficoLine';
 
+import api from '../../../../server/index';
+
 function Box() {
+  const [data,setData] = useState();
+    useEffect(()=>{
+        async function getData(){
+            try{
+                let response = await api.get('/cam/averagePerDay');
+                console.log(response);
+                setData(response.data);
+            }catch(error){
+                console.log(error);
+            }
+        }
+        getData();
+    });
+
   return (<Container>
     <Row>
       <Card />
     </Row>
      <Row>
       <GraficoLine />
-    </Row> 
+    </Row>
+    <Row>
+  <h1>Média Diária: {data.toFixed(2)} pessoas</h1>
+    </Row>
   </Container>);
 }
 
